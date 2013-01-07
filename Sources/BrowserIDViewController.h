a2a8270606358e9425d2aad1881ec093af5bb6c5
Jens Alfke <jens@couchbase.com>
2013-01-06 16:09:42 -0800

    Code cleanup and header-doc comments.

--- Sources/BrowserIDViewController.h
+++ Sources/BrowserIDViewController.h
@@ -4,13 +4,34 @@
 
 @class BrowserIDViewController;
 
+/** Delegate of a BrowserIDViewController. Of the optional methods, didSucceedWithAssertion
+    must be implemented unless the controller's 'verifier' property is set, in which case both
+    didSucceedVerificationWithReceipt and didFailVerificationWithError must be implemented. */
 @protocol BrowserIDViewControllerDelegate <NSObject>
+
+/** Sent if the user presses the Cancel button on the BrowserID window. */
 - (void) browserIDViewControllerDidCancel: (BrowserIDViewController*) browserIDViewController;
-- (void) browserIDViewController: (BrowserIDViewController*) browserIDViewController didFailWithReason: (NSString*) reason;
+
+/** Sent if the authentication process fails. Currently the reason will just be @"". */
+- (void) browserIDViewController: (BrowserIDViewController*) browserIDViewController
+               didFailWithReason: (NSString*) reason;
 @optional
-- (void) browserIDViewController: (BrowserIDViewController*) browserIDViewController didSucceedWithAssertion: (NSString*) assertion;
-- (void) browserIDViewController: (BrowserIDViewController*) browserIDViewController didSucceedVerificationWithReceipt: (NSDictionary*) receipt;
-- (void) browserIDViewController: (BrowserIDViewController*) browserIDViewController didFailVerificationWithError: (NSError*) error;
+/** Sent after authentication was successful. The assertion will be a long opaque string that
+    should be sent to the origin site's BrowserID authentication API. */
+- (void) browserIDViewController: (BrowserIDViewController*) browserIDViewController
+         didSucceedWithAssertion: (NSString*) assertion;
+
+/** Sent after authentication and server-side verification are successful, _only_ if the
+    controller's 'verifier' property is set to a server-side verifier URL.
+    The 'receipt' parameter is the verifier response as decoded from JSON. */
+- (void) browserIDViewController: (BrowserIDViewController*) browserIDViewController
+         didSucceedVerificationWithReceipt: (NSDictionary*) receipt;
+
+/** Sent if server-side verification fails, _only_ if the controller's 'verifier' property is set
+    to a server-side verifier URL. */
+- (void) browserIDViewController: (BrowserIDViewController*) browserIDViewController
+    didFailVerificationWithError: (NSError*) error;
+
 @end
 
 @interface BrowserIDViewController : UIViewController <UIWebViewDelegate> {
@@ -18,11 +39,23 @@
 
 @property (nonatomic,strong) IBOutlet UIWebView* webView;
 
+/** The object that will be informed about success or failure. Required. */
 @property (nonatomic,weak) id<BrowserIDViewControllerDelegate> delegate;
-@property (nonatomic,strong) NSString* origin;
+
+/** The URL of the site the user is logging into (i.e. the site you will send the assertion to).
+	Required. */
+@property (nonatomic,strong) NSURL* origin;
+
+/** An optional URL of a verification service provided by your applicatin's server-side counterpart.
+	If this property is set, an assertion will be sent to this URL as the body of a POST request,
+	and the response relayed to the delegate via its verification-related methods. */
 @property (nonatomic,strong) NSURL* verifier;
 
 /** After a successful login, this property will be set to the email address the user entered. */
 @property (nonatomic,strong) NSString* emailAddress;
+
+/** A convenience method that puts the receiver in a UINavigationController and presents it modally
+    in the given parent controller. */
+- (UINavigationController*) presentModalInController: (UIViewController*)parentController;
 
 @end
--- Sources/BrowserIDViewController.m
+++ Sources/BrowserIDViewController.m
@@ -16,147 +16,157 @@ static NSString* const kBrowserIDSignInURL = @"https://login.persona.org/sign_in
 @synthesize verifier = _verifier;
 @synthesize emailAddress = _emailAddress;
 
+- (UINavigationController*) presentModalInController: (UIViewController*)parentController {
+	UINavigationController* navController = [[UINavigationController alloc]
+													initWithRootViewController: self];
+	if (navController == nil)
+		return nil;
+
+	if (UI_USER_INTERFACE_IDIOM() != UIUserInterfaceIdiomPhone) {
+		navController.modalPresentationStyle = UIModalPresentationFormSheet;
+	}
+	[parentController presentViewController: navController animated: YES completion: nil];
+	return navController;
+}
+
 - (void) viewDidLoad
 {
-    [super viewDidLoad];
-    
-    self.title = @"BrowserID";
-        
-    UIBarButtonItem* cancelButton = [[UIBarButtonItem alloc] initWithTitle: @"Cancel"
-        style: UIBarButtonItemStylePlain target: self action: @selector(cancel)];
+	[super viewDidLoad];
+	
+	self.title = NSLocalizedString(@"Log In With Persona", @"BrowserID login window title");
+		
+	UIBarButtonItem* cancelButton = [[UIBarButtonItem alloc] initWithTitle: @"Cancel"
+		style: UIBarButtonItemStylePlain target: self action: @selector(cancel)];
 #if !__has_feature(objc_arc)
-    [cancelButton autorelease];
+	[cancelButton autorelease];
 #endif
 
-    self.navigationItem.rightBarButtonItem = cancelButton;
+	self.navigationItem.rightBarButtonItem = cancelButton;
 
-    _webView.delegate = self;
+	_webView.delegate = self;
 
 	// Insert the code that will setup and handle the BrowserID callback.
 
 	NSString* injectedCodePath = [[NSBundle mainBundle] pathForResource: @"BrowserIDViewController" ofType: @"js"];
 	NSString* injectedCodeTemplate = [NSString stringWithContentsOfFile: injectedCodePath encoding:NSUTF8StringEncoding error: nil];
-    if (injectedCodeTemplate == nil) {
-        NSLog(@"Could not load BrowserIDViewController.js");
-        return;
-    }
-    
-	NSString* injectedCode = [NSString stringWithFormat: injectedCodeTemplate, _origin];
+	NSAssert(injectedCodeTemplate != nil, @"Could not load BrowserIDViewController.js");
+
+	NSString* injectedCode = [NSString stringWithFormat: injectedCodeTemplate, _origin.absoluteString];
 
 	[_webView stringByEvaluatingJavaScriptFromString: injectedCode];
 }
 
 - (void) viewWillAppear:(BOOL)animated
 {
-    [_webView loadRequest: [NSURLRequest requestWithURL: [NSURL URLWithString: kBrowserIDSignInURL]]];
+	[_webView loadRequest: [NSURLRequest requestWithURL: [NSURL URLWithString: kBrowserIDSignInURL]]];
 }
 
 - (void) viewDidUnload
 {
-    [super viewDidUnload];
+	[super viewDidUnload];
 }
 
 - (BOOL) shouldAutorotateToInterfaceOrientation: (UIInterfaceOrientation) interfaceOrientation
 {
-    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
-        return (interfaceOrientation == UIInterfaceOrientationPortrait);
-    } else {
-        return YES;
-    }
+	if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+		return (interfaceOrientation == UIInterfaceOrientationPortrait);
+	} else {
+		return YES;
+	}
 }
 
 #pragma mark -
 
 - (IBAction) cancel
 {
-    [_webView stopLoading];
-    [_delegate browserIDViewControllerDidCancel: self];
+	[_webView stopLoading];
+	[_delegate browserIDViewControllerDidCancel: self];
 }
 
 #pragma mark -
 
 - (void) verifyAssertion: (NSString*) assertion
 {
-    // POST the assertion to the verification endpoint. Then report back to our delegate about the
-    // results.
-    
-    id verifyCompletionHandler = ^(NSHTTPURLResponse* response, NSData* data, NSError* error)
-    {
-        if (error) {
-            [_delegate browserIDViewController: self didFailVerificationWithError: error];
-        } else {
-            NSError* decodingError = nil;
-            NSDictionary* receipt = [NSJSONSerialization JSONObjectWithData: data options: 0 error: &decodingError];
-            if (decodingError) {
-                [_delegate browserIDViewController: self didFailVerificationWithError: decodingError];
-            } else {
-                [_delegate browserIDViewController: self didSucceedVerificationWithReceipt: receipt];
-            }
-        }
-    };
-    
-    NSMutableURLRequest* request = [[NSMutableURLRequest alloc] initWithURL: self.verifier cachePolicy: NSURLCacheStorageAllowed timeoutInterval: 5.0];
+	// POST the assertion to the verification endpoint. Then report back to our delegate about the
+	// results.
+	
+	id verifyCompletionHandler = ^(NSHTTPURLResponse* response, NSData* data, NSError* error)
+	{
+		if (error) {
+			[_delegate browserIDViewController: self didFailVerificationWithError: error];
+		} else {
+			NSError* decodingError = nil;
+			NSDictionary* receipt = [NSJSONSerialization JSONObjectWithData: data options: 0 error: &decodingError];
+			if (decodingError) {
+				[_delegate browserIDViewController: self didFailVerificationWithError: decodingError];
+			} else {
+				[_delegate browserIDViewController: self didSucceedVerificationWithReceipt: receipt];
+			}
+		}
+	};
+	
+	NSMutableURLRequest* request = [[NSMutableURLRequest alloc] initWithURL: self.verifier cachePolicy: NSURLCacheStorageAllowed timeoutInterval: 5.0];
 #if !__has_feature(objc_arc)
-    [request autorelease];
+	[request autorelease];
 #endif
-    [request setHTTPShouldHandleCookies: YES];
-    [request setHTTPMethod: @"POST"];
-    [request setHTTPBody: [assertion dataUsingEncoding: NSUTF8StringEncoding]];
-    [request setValue: @"text/plain" forHTTPHeaderField: @"content-type"];
-    
-    [NSURLConnection sendAsynchronousRequest: request queue: [NSOperationQueue mainQueue]
-        completionHandler: verifyCompletionHandler];
+	[request setHTTPShouldHandleCookies: YES];
+	[request setHTTPMethod: @"POST"];
+	[request setHTTPBody: [assertion dataUsingEncoding: NSUTF8StringEncoding]];
+	[request setValue: @"text/plain" forHTTPHeaderField: @"content-type"];
+	
+	[NSURLConnection sendAsynchronousRequest: request queue: [NSOperationQueue mainQueue]
+		completionHandler: verifyCompletionHandler];
 }
 
 - (BOOL) webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
 {
 	NSURL* url = [request URL];
-    
+	
 	// The JavaScript side (the code injected in viewDidLoad will make callbacks to this native code by requesting
 	// a BrowserIDViewController://callbackname/callback?data=foo style URL. So we capture those here and relay
 	// them to our delegate.
 	
 	if ([[[url scheme] lowercaseString] isEqualToString: @"browseridviewcontroller"])
-	{	
-        NSString* message = url.host;
-        NSString* param = [[url query] substringFromIndex: [@"data=" length]];
-        NSLog(@"MESSAGE '%@', param '%@'", message, param);
+	{
+		NSString* message = url.host;
+		NSString* param = [[url query] substringFromIndex: [@"data=" length]];
+		NSLog(@"MESSAGE '%@', param '%@'", message, param);
 		if ([message isEqualToString: @"assertionReady"]) {
-            NSRange separator = [param rangeOfString: @"&email="];
-            if (separator.length > 0) {
-                NSString* email = [param substringFromIndex: NSMaxRange(separator)];
-                param = [param substringToIndex: separator.location];
-                self.emailAddress = [email stringByReplacingPercentEscapesUsingEncoding: NSUTF8StringEncoding];
-            }
-            param = [param stringByReplacingPercentEscapesUsingEncoding: NSUTF8StringEncoding];
-            if (_verifier) {
-                [self verifyAssertion: param];
-            } else {
-                [_delegate browserIDViewController: self didSucceedWithAssertion: param];
-            }
-            }
+			NSRange separator = [param rangeOfString: @"&email="];
+			if (separator.length > 0) {
+				NSString* email = [param substringFromIndex: NSMaxRange(separator)];
+				param = [param substringToIndex: separator.location];
+				self.emailAddress = [email stringByReplacingPercentEscapesUsingEncoding: NSUTF8StringEncoding];
+			}
+			param = [param stringByReplacingPercentEscapesUsingEncoding: NSUTF8StringEncoding];
+			if (_verifier) {
+				[self verifyAssertion: param];
+			} else {
+				[_delegate browserIDViewController: self didSucceedWithAssertion: param];
+			}
+		}
 
 		else if ([message isEqualToString: @"assertionFailure"]) {
 			[_delegate browserIDViewController: self didFailWithReason: param];
-            }
+		}
 
 		else if ([message isEqualToString: @"gotEmail"]) {
 			self.emailAddress = param;
 		}
-	
+
 		return NO;
 	}
-    
-    // If the user clicked on a link that escapes the browserid dialog, then we open it in Safari
-    
-    else if ([[[url scheme] lowercaseString] isEqualToString: @"http"] || [[[url scheme] lowercaseString] isEqualToString: @"https"])
-    {
-        if ([[url absoluteString] isEqualToString: kBrowserIDSignInURL] == NO)
-        {
-            [[UIApplication sharedApplication] openURL: url];
-            return NO;
-        }
-    }
+	
+	// If the user clicked on a link that escapes the browserid dialog, then we open it in Safari
+	
+	else if ([[[url scheme] lowercaseString] isEqualToString: @"http"] || [[[url scheme] lowercaseString] isEqualToString: @"https"])
+	{
+		if ([[url absoluteString] isEqualToString: kBrowserIDSignInURL] == NO)
+		{
+			[[UIApplication sharedApplication] openURL: url];
+			return NO;
+		}
+	}
 	
 	return YES;
 }
